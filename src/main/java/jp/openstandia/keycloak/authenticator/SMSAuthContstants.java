a99660e1d68efbb9ddee33901bcd39025b792c11
Mohammed Ghawanni <m7md.ghawanni@gmail.com>
2020-07-19 12:07:25 +0300

    added required constants

--- src/main/java/jp/openstandia/keycloak/authenticator/SMSAuthContstants.java
+++ src/main/java/jp/openstandia/keycloak/authenticator/SMSAuthContstants.java
@@ -2,7 +2,13 @@ package jp.openstandia.keycloak.authenticator;
 
 public class SMSAuthContstants {
 
+
+	public static final String ACCOUNT_SID = "AC06d427143c56e331c217306433c9bf59";
+	public static final String AUTH_TOKEN = "phoneNumber";
+	public static final String AUTHY_API_KEY = "6toDrCvM9YnFBMCLWfHpPJYyb6N4B23P";
+	
 	public static final String ATTR_PHONE_NUMBER = "phoneNumber";
+	public static final String ATTR_EMAIL = "email";
 
 	public static final String CONFIG_SMS_API_KEY = "verifySMS.api-key";
 	public static final String CONFIG_PROXY_FLAG = "verifySMS.proxy-flg";
