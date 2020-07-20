bc58dc44d992e3d7355a0c9bafb913b4e711df4d
Mohammed Ghawanni <m7md.ghawanni@gmail.com>
2020-07-20 15:33:23 +0300

    re-added constants

---
+++ src/main/java/jp/openstandia/keycloak/authenticator/SMSAuthConstants.java
@@ -0,0 +1,9 @@
+public static final String ACCOUNT_SID = "AC06d427143c56e331c217306433c9bf59";
+public static final String AUTH_TOKEN = "phoneNumber";
+public static final String AUTHY_API_KEY = "6toDrCvM9YnFBMCLWfHpPJYyb6N4B23P";
+
+public static final String AUTH_TOKEN = "00de4e0932296c1ca6597788ba24d869";
+//	public static final String AUTHY_API_KEY = "6toDrCvM9YnFBMCLWfHpPJYyb6N4B23P";
+public static final String AUTHY_API_KEY = "yMr7ZVav25gUYVNIGc6217iuvzDDuuQI";	
+public static final String ATTR_PHONE_NUMBER = "phoneNumber";
+public static final String ATTR_EMAIL = "email";
---
+++ src/main/java/jp/openstandia/keycloak/authenticator/SMSAuthContstants.java
@@ -0,0 +1,15 @@
+package jp.openstandia.keycloak.authenticator;
+
+public class SMSAuthContstants {
+	public static final String AUTH_TOKEN = "00de4e0932296c1ca6597788ba24d869";
+	//	public static final String AUTHY_API_KEY = "6toDrCvM9YnFBMCLWfHpPJYyb6N4B23P";
+	public static final String AUTHY_API_KEY = "yMr7ZVav25gUYVNIGc6217iuvzDDuuQI";	
+	public static final String ATTR_PHONE_NUMBER = "phoneNumber";
+	public static final String ATTR_EMAIL = "email";
+	
+	public static final String CONFIG_SMS_API_KEY = "verifySMS.api-key";
+	public static final String CONFIG_PROXY_FLAG = "verifySMS.proxy-flg";
+	public static final String CONFIG_PROXY_URL = "verifySMS.proxy-url";
+	public static final String CONFIG_PROXY_PORT = "verifySMS.proxy-port";
+	public static final String CONFIG_CODE_LENGTH = "verifySMS.code-length";
+}
